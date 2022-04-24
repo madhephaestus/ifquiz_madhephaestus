6d33722e060d690b3cd8906772d357d4deefb748
Kevin Harrington <mad.hephaestus@gmail.com>
2022-04-24 12:59:25 -0400

    update

--- ifquiz.groovy
+++ ifquiz.groovy
@@ -1,10 +1,11 @@
-int x = 2**2;
+int x = 2*2;
+println x
 if(x > 4)
 {
   System.out.println("Does this work?");
 }
 int i = 9;
-if(i % 2 == 1)
+if( i < 10 )
 {
   System.out.println("Does this work?");
 }
